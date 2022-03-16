e89f3e1084c264253d4e8e52bd41a21127d3b7b4
Johansson Johannes <johannes.johansson@smhi.se>
2022-03-16 10:08:58 +0100

    Update get_file_as_string.py

--- example/get_file_as_string.py
+++ example/get_file_as_string.py
@@ -12,7 +12,7 @@ import pandas as pd
 import time
 
 if __name__ == '__main__':
-    # url = r'http://10.64.10.247:5000/getfile'
+    url = r'http://10.64.10.247:5000/getfile'
     start_time = time.time()
     response = requests.request("GET", url)
     print("Timeit:--%.5f sec" % (time.time() - start_time))
